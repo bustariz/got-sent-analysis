19ee8a6b0850cfcda20cd1b696b723c801deb9ac
Bruno Ustariz <b@bmbp.local>
2020-09-01 22:49:53 -0400

    update to connection for postgresql db in app

--- app/app.py
+++ app/app.py
@@ -1,121 +1,173 @@
-from flask import Flask,render_template,jsonify
-from flask_pymongo import PyMongo
-from bson.json_util import dumps, loads 
-from flask_cors import CORS
-import data
+import numpy as np
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
 
+from flask import Flask, render_template, jsonify
+
+# from flask_cors import CORS
+# import data
+
+#################################################
+# Database setup
+#################################################
+engine = create_engine('postgresql://postgres:bru1014ustz91@localhost/got_db')
+
+# reflect existing db into new model
+Base = automap_base()
+
+# reflect tables
+Base.prepare(engine,reflect = True)
+
+# # ref to the table
+# script = Base.classes.got_script
+
+#################################################
+# flask setup
+#################################################
 app = Flask(__name__)
-CORS(app)
+# CORS(app)
+
+#################################################
+#flask routes
+#################################################
+
+@app.route('/')
+def welcome():
+    """list all available API routes."""
+    # return 'Hello World!'
+    return (
+        f"Available Routes: <br/>"
+        f"/api/v1.0/text <br/>"
+        f'/api/v1.0/sentiment'
+    )
+
+# @app.route('/api/v1.0/text')
+# def text_analysis():
+#       # Create our session (link) from Python to the DB
+#     session = Session(engine)
+
+#     """Return a list of all passenger names"""
+#     # Query all passengers
+#     results = session.query(script_table).all()
+
+#     session.close()
+
+#     all_names = list(np.ravel(results))
+
+#     return jsonify(all_names)
+
 
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/sent_analysisdb"
-mongo = PyMongo(app)
+# app.config["MONGO_URI"] = "mongodb://localhost:27017/sent_analysisdb"
+# mongo = PyMongo(app)
 
-data.run_functions()
+# data.run_functions()
 
-@app.route("/api/v1.0/text/season")
-def index():
-    collection = mongo.db.got_scripts
+# @app.route("/api/v1.0/text/season")
+# def index():
+#     collection = mongo.db.got_scripts
 
-    pipeline = [
-        {
-            "$group":{
-                "_id":"$name",
-                "total_word_count": {"$sum":"$word_count"},
-                "avg_compound_score": {"$avg":"$compound_score"}
-            }
-        },
-        {
-            "$sort":{"total_word_count":-1}
-        },
-        {
-            "$limit": 100
-        }
-    ]
+#     pipeline = [
+#         {
+#             "$group":{
+#                 "_id":"$name",
+#                 "total_word_count": {"$sum":"$word_count"},
+#                 "avg_compound_score": {"$avg":"$compound_score"}
+#             }
+#         },
+#         {
+#             "$sort":{"total_word_count":-1}
+#         },
+#         {
+#             "$limit": 100
+#         }
+#     ]
 
-    chars_word_count = collection.aggregate(pipeline=pipeline)
+#     chars_word_count = collection.aggregate(pipeline=pipeline)
     
-    output = []
-    for item in chars_word_count:
-        output.append({
-            "name":item["_id"],
-            "total_word_count":item["total_word_count"],
-            "avg_compound_score": item["avg_compound_score"]
-            })
+#     output = []
+#     for item in chars_word_count:
+#         output.append({
+#             "name":item["_id"],
+#             "total_word_count":item["total_word_count"],
+#             "avg_compound_score": item["avg_compound_score"]
+#             })
 
-    return jsonify({"results": output})
+#     return jsonify({"results": output})
 
 
 
-@app.route("/api/v1.0/text/season/<string:season_num>")
-def season_text(season_num):
+# @app.route("/api/v1.0/text/season/<string:season_num>")
+# def season_text(season_num):
 
-    collection = mongo.db.got_scripts
+#     collection = mongo.db.got_scripts
 
-    pipeline = [
-        {
-            "$match":{"season":f"Season {season_num}"}
-        },
-        {
-            "$group":{
-                "_id":"$name",
-                "total_word_count": {"$sum":"$word_count"},
-                "avg_compound_score": {"$avg":"$compound_score"}
-            }
-        },
-        {
-            "$sort":{"total_word_count":-1}
-        },
-        {
-            "$limit": 100
-        }
-    ]
+#     pipeline = [
+#         {
+#             "$match":{"season":f"Season {season_num}"}
+#         },
+#         {
+#             "$group":{
+#                 "_id":"$name",
+#                 "total_word_count": {"$sum":"$word_count"},
+#                 "avg_compound_score": {"$avg":"$compound_score"}
+#             }
+#         },
+#         {
+#             "$sort":{"total_word_count":-1}
+#         },
+#         {
+#             "$limit": 100
+#         }
+#     ]
 
-    chars_word_count = collection.aggregate(pipeline=pipeline)
+#     chars_word_count = collection.aggregate(pipeline=pipeline)
     
-    output = []
-    for item in chars_word_count:
-        output.append({
-            "name":item["_id"],
-            "total_word_count":item["total_word_count"],
-            "avg_compound_score": item["avg_compound_score"]
-            })
+#     output = []
+#     for item in chars_word_count:
+#         output.append({
+#             "name":item["_id"],
+#             "total_word_count":item["total_word_count"],
+#             "avg_compound_score": item["avg_compound_score"]
+#             })
 
-    return jsonify({"results": output})
+#     return jsonify({"results": output})
 
-@app.route("/api/v1.0/text/season/all")
-def all_season_text():
+# @app.route("/api/v1.0/text/season/all")
+# def all_season_text():
     
-    collection = mongo.db.got_scripts
+#     collection = mongo.db.got_scripts
 
-    pipeline = [
-        {
-            "$group":{
-                "_id":"$season",
-                "total_word_count": {"$sum":"$word_count"},
-                "avg_compound_score": {"$avg":"$compound_score"}
-            }
-        },
-        {
-            "$sort":{"total_word_count":-1}
-        },
-        {
-            "$limit": 100
-        }
-    ]
+#     pipeline = [
+#         {
+#             "$group":{
+#                 "_id":"$season",
+#                 "total_word_count": {"$sum":"$word_count"},
+#                 "avg_compound_score": {"$avg":"$compound_score"}
+#             }
+#         },
+#         {
+#             "$sort":{"total_word_count":-1}
+#         },
+#         {
+#             "$limit": 100
+#         }
+#     ]
 
-    chars_word_count = collection.aggregate(pipeline=pipeline)
+#     chars_word_count = collection.aggregate(pipeline=pipeline)
     
-    output = []
-    for item in chars_word_count:
-        output.append({
-            "season":item["_id"],
-            "total_word_count":item["total_word_count"],
-            "avg_compound_score": item["avg_compound_score"]
-            })
+#     output = []
+#     for item in chars_word_count:
+#         output.append({
+#             "season":item["_id"],
+#             "total_word_count":item["total_word_count"],
+#             "avg_compound_score": item["avg_compound_score"]
+#             })
 
-    return jsonify({"results": output})
+#     return jsonify({"results": output})
 
 
 
--- app/data.py
+++ app/data.py
@@ -1,45 +1,31 @@
-# from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import pandas as pd
-import numpy as np
+# import numpy as np
 from textblob import TextBlob
 from sqlalchemy import create_engine
+import psycopg2
 
-
+# engine to connect to postgresql db
 engine = create_engine('postgresql://postgres:bru1014ustz91@localhost/got_db')
 
 # # Reads in script from csv file and renames columns to lowercase
-# path = '../data/Game_of_Thrones_Script.csv'
+path = '../data/Game_of_Thrones_Script.csv'
 
-# script_df = pd.read_csv(path)
+script_df = pd.read_csv(path)
 
-# script_df = script_df.rename(columns = {'Release Date': 'release_date',
-#                                         'Season': 'season',
-#                                         'Episode':'episode',
-#                                         'Episode Title':'episode_title',
-#                                         'Name':'name', 
-#                                         'Sentence': 'sentence'
-#                                        })
+script_df = script_df.rename(columns = {'Release Date': 'release_date',
+                                        'Season': 'season',
+                                        'Episode':'episode',
+                                        'Episode Title':'episode_title',
+                                        'Name':'name', 
+                                        'Sentence': 'sentence'
+                                       })
 
+text_blobs = [TextBlob(sentence) for sentence in script_df.sentence]
 
+script_df['tokenized_words'] = [sentence.words for sentence in text_blobs]
+script_df['alpha_numeric_words'] = script_df.tokenized_words.apply(lambda x: [item for item in x if item.isalnum()])
+script_df['word_count'] = [len(words) for words in script_df.alpha_numeric_words ]
+script_df['polarity_score'] = [sentence.sentiment.polarity for sentence in text_blobs]
+script_df['subjectivity_score'] = [sentence.sentiment.subjectivity for sentence in text_blobs]
 
-
-# text_blobs = [TextBlob(sentence) for sentence in script_df.sentence]
-
-
-
-# script_df['tokenized_words'] = [sentence.words for sentence in text_blobs]
-# script_df['alpha_numeric_words'] = script_df.tokenized_words.apply(lambda x: [item for item in x if item.isalnum()])
-# script_df['word_count'] = [len(words) for words in script_df.alpha_numeric_words ]
-# script_df['polarity_score'] = [sentence.sentiment.polarity for sentence in text_blobs]
-# script_df['subjectivity_score'] = [sentence.sentiment.subjectivity for sentence in text_blobs]
-
-
-# script_df.to_sql('got_script',engine)
-
-
-
-
-
-
-
-
+script_df.to_sql('got_script',engine)
